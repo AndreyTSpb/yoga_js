8575b0aee7974f0a7b35e396f8fc80cb04b9a6c7
AndreyTSpb <tynyanyi@mail.ru>
2020-01-10 15:02:13 +0300

    cout down

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
-    function func(menu, menuItems, tabs){
+    function showBlock(menu, menuItems, tabs){
         let tab = document.querySelectorAll("."+menuItems),
             info = document.querySelector("."+menu),
             tabContent = document.querySelectorAll("."+tabs);
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function(){
         info.addEventListener('click', function(event){
             let target = event.target;
             //проверка действительно ли это меню табов
-            if(target && target.classList.contains('info-header-tab')){
+            if(target && target.classList.contains(menuItems)){
                 //перебираем все табы и сравниваем с нажатым
                 for(let i = 0; i < tab.length; i++){
                     //если совпало то скрыть все, но открыть нажатый
@@ -36,7 +36,84 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         });
     }
+    function sleep(milliseconds) {
+        const date = Date.now();
+        let currentDate = null;
+        do {
+          currentDate = Date.now();
+        } while (currentDate - date < milliseconds);
+    }
 
-    func('info-header', 'info-header-tab', 'info-tabcontent');
-    
+    function countDown(){
+        /**
+         * Плейс холдеры для счетчика  
+         */
+        let ph_hour = document.querySelector('#timer .hours'),
+            ph_min  = document.querySelector('#timer .minutes'),
+            ph_sec  = document.querySelector('#timer .seconds');
+
+        /**
+         * Заполняем значения переменных из плейсхолдеров с сайта,
+         * преобразуем их к числу
+         * и конвертируем это значение в Unix TimeStamps путем банального сложения умножения
+         */    
+        let hour = +ph_hour.textContent,
+            min  = +ph_min.textContent, 
+            sec  = +ph_sec.textContent, 
+            timestamp = hour*60*60 + min*60 + sec; 
+        
+        /**
+         * Проверяем если время больше одной секунды то запускаем счетчик иначе показываем нули
+         */
+        if(timestamp > 1){
+            /**
+             * Запускаем цыкл счетчика
+             * Конец цыкла как счетчик достигнет timestamp
+             */
+            for (let i = 0; i < timestamp; i++) {
+                /**
+                 * С каждой итерацией цикла 
+                 * задержку умножаем на счетчик цикла 
+                 * и прибавляем одну милисекунду
+                 */
+                setTimeout(function () {
+                    sec--;
+                    if(sec < 0 ){
+                        sec +=60;
+                        min --;
+                        if(min < 0){
+                            min += 60;
+                            hour --;
+                            if(hour<0){
+                                // если отнимать больше нечего то нули и прирываем выполнение функции
+                                ph_hour.textContent = "00";
+                                ph_min.textContent  = "00";
+                                ph_sec.textContent  = "00";
+                                return;
+                            }
+                        }
+                    }
+                    /**
+                     * Заполняем плейсхолдеры полученными значениями
+                     */
+                    ph_sec.textContent  = (sec < 10) ? "0" + sec : sec;
+                    ph_min.textContent  = (min < 10) ? "0" + min : min;
+                    ph_hour.textContent = (hour < 10) ? "0"+hour : hour;
+                }, 1000 * i + 1, i);
+            }
+        }else{
+            ph_hour.textContent = "00";
+            ph_min.textContent  = "00";
+            ph_sec.textContent  = "00";
+        }
+    }
+
+
+    /**
+     * Запуск все функций
+     */
+
+    //Отображаем блоки по очереди
+    showBlock('info-header', 'info-header-tab', 'info-tabcontent');
+    countDown();
 });
