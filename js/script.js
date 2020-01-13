b85d893878dea6093bf4ab9a00f01669d18d1a81
AndreyTSpb <tynyanyi@mail.ru>
2020-01-13 09:43:19 +0300

    cout down update

--- js/script.js
+++ js/script.js
@@ -44,69 +44,74 @@ window.addEventListener('DOMContentLoaded', function(){
         } while (currentDate - date < milliseconds);
     }
 
-    function countDown(){
+
+    //COUNTDOWN
+
+    /**
+     * Преобразуем время полученное от пользователя или со страницы
+     * в юниксовое время 
+     */
+    function getTime(endtime){
+        /**
+         * Получаем время для счетчика в секундах 
+         */
+        let t = (Date.parse(endtime) - Date.parse(new Date))/1000;
+        /**
+         * Получаем секунды минуты и часы до окончания счетчика
+         */
+        let sec = Math.floor(t % 60); // остаток от деления будут секундами
+        // endtime разделить на 60 секунд что бы получить количество минут и потом еще раз разделитиь на 60 чтобы получить остаток
+        let min = Math.floor((t/60) % 60);
+        // endtime надо разделить на 3600 
+        let hour = Math.floor(t/3600);
+        /**
+         * для получения дней 
+         * let hour = Math.floor((endtime/3600) % 24);
+         * let day = Math.floor(endtime/60*60*24);
+         */
+        /**
+         * Возвращаем объект
+         */
+        return {
+            'endtime': t,
+            'hours': hour,
+            'min': min,
+            'sec': sec
+        }
+    }
+
+    /**
+     * Заполняем плейсхолдеры данными
+     */
+    function setTimer(id, deadline){
         /**
          * Плейс холдеры для счетчика  
          */
-        let ph_hour = document.querySelector('#timer .hours'),
-            ph_min  = document.querySelector('#timer .minutes'),
-            ph_sec  = document.querySelector('#timer .seconds');
+        let timer = document.getElementById(id),
+            ph_hour = timer.querySelector('.hours'),
+            ph_min  = timer.querySelector('.minutes'),
+            ph_sec  = timer.querySelector('.seconds'),
+            timerInterval = setInterval(updateClock,1000);
 
-        /**
-         * Заполняем значения переменных из плейсхолдеров с сайта,
-         * преобразуем их к числу
-         * и конвертируем это значение в Unix TimeStamps путем банального сложения умножения
-         */    
-        let hour = +ph_hour.textContent,
-            min  = +ph_min.textContent, 
-            sec  = +ph_sec.textContent, 
-            timestamp = hour*60*60 + min*60 + sec; 
-        
-        /**
-         * Проверяем если время больше одной секунды то запускаем счетчик иначе показываем нули
-         */
-        if(timestamp > 1){
-            /**
-             * Запускаем цыкл счетчика
-             * Конец цыкла как счетчик достигнет timestamp
-             */
-            for (let i = 0; i < timestamp; i++) {
-                /**
-                 * С каждой итерацией цикла 
-                 * задержку умножаем на счетчик цикла 
-                 * и прибавляем одну милисекунду
-                 */
-                setTimeout(function () {
-                    sec--;
-                    if(sec < 0 ){
-                        sec +=60;
-                        min --;
-                        if(min < 0){
-                            min += 60;
-                            hour --;
-                            if(hour<0){
-                                // если отнимать больше нечего то нули и прирываем выполнение функции
-                                ph_hour.textContent = "00";
-                                ph_min.textContent  = "00";
-                                ph_sec.textContent  = "00";
-                                return;
-                            }
-                        }
-                    }
-                    /**
-                     * Заполняем плейсхолдеры полученными значениями
-                     */
-                    ph_sec.textContent  = (sec < 10) ? "0" + sec : sec;
-                    ph_min.textContent  = (min < 10) ? "0" + min : min;
-                    ph_hour.textContent = (hour < 10) ? "0"+hour : hour;
-                }, 1000 * i + 1, i);
+        //Обновление времени на странице
+        function updateClock(){
+            let t = getTime(deadline);
+            ph_hour.textContent = (t.hours<10)?"0"+t.hours:t.hours;
+            ph_min.textContent  = (t.min<10)?"0"+t.min:t.min;
+            ph_sec.textContent  = (t.sec<10)?"0"+t.sec:t.sec;
+
+            //прерываем таймер 
+            if(t.endtime <= 0 ){
+                clearInterval(timerInterval);
+                // Ставим нули
+                ph_hour.textContent = "00";
+                ph_min.textContent  = "00";
+                ph_sec.textContent  = "00";
             }
-        }else{
-            ph_hour.textContent = "00";
-            ph_min.textContent  = "00";
-            ph_sec.textContent  = "00";
         }
-    }
+    };
+    
+
 
 
     /**
@@ -115,5 +120,5 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //Отображаем блоки по очереди
     showBlock('info-header', 'info-header-tab', 'info-tabcontent');
-    countDown();
+    setTimer('timer','2020-01-20');
 });
