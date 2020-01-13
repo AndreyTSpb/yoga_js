cdba9c85acc426605560f3eef59f6fb577fb0931
AndreyTSpb <tynyanyi@mail.ru>
2020-01-13 13:19:20 +0300

    Show modal v1

--- js/script.js
+++ js/script.js
@@ -110,6 +110,62 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     };
+
+
+    //MODAL WINDOW "GET MORE"
+    /**
+     * Открытие модального окна
+     * Аргумент это класс модалки, чтобы можно было вызывать универсально
+     */
+    function showModal(name_modal){
+        /**
+         * Получаем ссылку на модалку по ее классу.(находит первый элемент с таким классом на странице)
+         */
+        let modal = document.querySelector('#'+name_modal);
+
+        /**
+         * меняем значение дисплей на block
+         */
+        modal.style.display = 'block';
+
+        /**
+         * Получаем кнопку закрыть для этой модалки
+         */
+        let close = modal.querySelector('.popup-close');
+        close.addEventListener('click', closeModal.bind(null, name_modal)); //передаем параметры в функцию без ее вызова
+    }
+
+    /**
+     * Закрытие модального окна
+     */
+    function closeModal(name_modal){
+        let modal = document.querySelector('#'+name_modal);
+        modal.style.display = 'none';
+    }
+
+    /**
+     * Обработка нажания кнопки для запуска модалки
+     * param@ name_button
+     */
+    function btnShowModal(name_button){
+        /**
+         * Получаем все кнопки с указанным классом 
+         */
+        let btns = document.querySelectorAll('.'+name_button);
+        /**
+         * навешиваем слушателя для каждой кнопки в массиве
+         */
+        for(let btn of btns){
+            btn.addEventListener('click', clikBtnModalShow);
+        }
+
+        function clikBtnModalShow(){
+            let name_modal = this.getAttribute('data-modal');
+            if(name_modal != null && name_modal.length > 0) showModal(name_modal);
+        }
+    }
+
+    btnShowModal('more');
     
 
 
